1300fd9f284dcb0167f5f934963efb6b3b306fb6
lucianS <l.stoica.extern@instamotion.com>
2023-12-09 16:00:47 +0200

    code gen comand

--- packages/diff/src/index.ts
+++ packages/diff/src/index.ts
@@ -1,12 +1,59 @@
-import http from 'node:http';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-const server = http.createServer((req, resp) => {
 
-    resp.writeHead(201, { 'Content-Type': 'text/plain' });
-    resp.end('ok\n');
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+
+const resolvers = {
+    Query: {
+        books: () => books,
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+async function  main() {
+    const { url } = await startStandaloneServer(server, {
+      listen: { port: 4000 },
+    });
+
+    return url;
+}
+
+main().then((url: string) => {
+
+    console.log(`🚀  Server ready at: ${url}`);
+}).catch((err) => {
+    console.log(err, ' not able to start the server');
 });
-
-server.listen(3001, '127.0.0.1', () => {
-    console.log('server is up');
-});
-
+  
